c913464b21299b73d3c6b62be302973f3a97cd50
felipebottega <felipebottega@gmail.com>
2019-08-17 22:16:38 -0300

    Fixed bug in test_tensors function

--- modules/Display.py
+++ modules/Display.py
@@ -332,16 +332,16 @@ def test_tensors(tensors_list, options_list, trials, display):
         maxiter = output.options.maxiter
         tol = output.options.tol
         init = output.options.initialization
-        if output.options.inner_method == 'cg':
+        if output.options.method == 'als' or output.options.method == 'ttcpd':
+            temp1 = [output.options.method, '', '']
+        elif output.options.inner_method == 'cg':
             temp1 = [output.options.inner_method, output.options.cg_factor, output.options.cg_tol]
         elif output.options.inner_method == 'cg_static':
             temp1 = [output.options.inner_method, output.options.cg_maxiter, output.options.cg_tol]
         elif output.options.inner_method == 'direct':
             temp1 = [output.options.inner_method, '', '']
-        elif output.options.inner_method == 'cg':
+        elif output.options.inner_method == 'gd':
             temp1 = [output.options.inner_method, '', '']
-        elif output.options.method == 'als' or output.options.method == 'ttcpd':
-            temp1 = [output.options.method, '', '']
         if T.ndim > 3:
             temp2 = output.options.bicpd_method_parameters
         else:
