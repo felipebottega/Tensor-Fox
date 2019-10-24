c6d7ae22592d871c1e8eb13a19106c3e4af8461e
felipebottega <felipebottega@gmail.com>
2019-10-23 22:04:22 -0300

    Updated

--- tests/swamps.py
+++ tests/swamps.py
@@ -32,7 +32,7 @@ def create(m, n, p, r, c, var):
         Y[:, l] = Q_Y[:, 1] + c*Q_Y[:, l]
         Z[:, l] = Q_Z[:, 2] + c*Q_Z[:, l]
     
-    T = tfx.cnv.cpd2tens(T, (X, Y, Z), (m, n, p))        
+    T = tfx.cnv.cpd2tens([X, Y, Z])        
     T_noise = T + var*np.random.randn(m, n, p)
 
     return T, T_noise
