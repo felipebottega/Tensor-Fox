4a0d91c8bb5b03b6247a7776662cbaa16fad19f6
unknown <felipebottega@gmail.com>
2022-03-10 09:54:41 -0300

    Fixed dependencies versions on setup.py

--- modules/TensorFox/__init__.py
+++ modules/TensorFox/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 def version():
-    return (1, 0, 5)
+    return (1, 0, 6)
 
 from .TensorFox import *
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='TensorFox',
-    version='1.0.5',
+    version='1.0.6',
     description='Tensor Fox is a high performance package of multilinear algebra and tensor routines, with focus on the Canonical Polyadic Decomposition (CPD), also called PARAFAC or CANDECOMP.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,14 +19,14 @@ setup(
     url='https://github.com/felipebottega/Tensor-Fox',
     keywords='Tensor Fox Canonical Polyadic Decomposition CPD PARAFAC CANDECOMP Multilinear Learning',
     install_requires=[
-          'numpy>=1.21.0',
-          'pandas>=1.2.3',
-          'scipy>=1.6.2',
-          'scikit-learn>=0.24.1',
-          'matplotlib>=3.3.4',
-          'numba>=0.53.1',
-          'IPython>=7.31.1',
-          'sparse_dot_mkl>=0.7',
+          'numpy==1.21.0',
+          'pandas==1.2.3',
+          'scipy==1.6.2',
+          'scikit-learn==0.24.1',
+          'matplotlib==3.3.4',
+          'numba==0.53.1',
+          'IPython==7.31.1',
+          'sparse_dot_mkl==0.7',
       ],
 
 )
